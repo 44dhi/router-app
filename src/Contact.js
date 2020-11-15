5747b83adb8417a07b4aad6872e4f47a0e46fcbc
Aadhit <aadhit@newslanded.com>
2020-11-15 17:02:49 +0530

    Routing App

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Got Questions? Contact us!</h2>
+                <p>Get me up on Instagram</p>
+                <p>@desigggnstudio</p>
+            </div>
+        )
+    }
+}
---
+++ src/Stuff.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+
+export default class Stuff extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Stuff</h2>
+                <p>This is just some stuff here and there</p>
+                <ul>
+                    <li>I'm learning react</li>
+                    <li>React is pretty cool</li>
+                </ul>
+            </div>
+        )
+    }
+}
